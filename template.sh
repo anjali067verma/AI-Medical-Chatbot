d6c4f4832b9ba07df2fb2957d5ae6b79635ed340
ANJALI VERMA <143899915+anjali067verma@users.noreply.github.com>
2025-12-13 18:12:10 +0530

    Project Structure Created Successfully

---
+++ template.sh
@@ -0,0 +1,17 @@
+#Creating directory structure for the project
+mkdir -p src
+mkdir -p research
+
+#Creating files in the respective directories
+touch src/__inti__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch store_index.py
+
+
+echo "Project structure created successfully."
